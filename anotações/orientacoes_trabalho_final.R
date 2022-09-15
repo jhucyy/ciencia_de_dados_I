4979d231624a0b5a45450b789c602a8fdf4f49b6
juciane <jhucyy.pereira@gmail.com>
2022-09-15 19:26:07 -0300

    orientações trabalho final

---
+++ anotações/orientacoes_trabalho_final.R
@@ -0,0 +1,17 @@
+
+# Anotações aula 15/09/2022 -----------------------------------------------
+
+# fazer um relatório em html de análise exploratória.
+# O relatório deve ter output em Rmarkdown.
+# O trabalho final deve ter uma breve descrição da 
+# base de dados. 
+# apresentar o objetivo da análise.
+# ideia, utilizar a minha base do mestrado.
+# apresentar, percentual de vídeos apagados.
+# frequência de postagem dos vídeos.
+# tipo de conteúdo apresentado. 
+# fazer uma conclusão geral do trabalho. 
+
+
+
+# data limite 30 de outubro.
